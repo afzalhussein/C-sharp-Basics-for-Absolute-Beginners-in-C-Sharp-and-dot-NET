cc27a791e9ce3568eae519a6afc4c868f03bb1db
Syed Hussain <afzalhussein@hotmail.com>
2024-09-27 09:30:24 -0400

    Exercise solution

--- Section 1 -GetStarted/Assignment1/Program.cs
+++ Section 1 -GetStarted/Assignment1/Program.cs
@@ -7,15 +7,15 @@ namespace Assignment1
         static void Main(string[] args)
         {
             // Exercise 1
-            Comment this line using single line comment. 
+            // Comment this line using single line comment. 
 
             // Exercise 2
-            Comment this and the next line using multiline comment. 
-            Visual Studio will show error if we dont comment normal text. 
+            /* Comment this and the next line using multiline comment. 
+            Visual Studio will show error if we dont comment normal text. */
 
             // Exercise 3
             // Write a C# statement to print your name to the console.
-            
+            Console.Write('Syed');
         }
     }
 }
